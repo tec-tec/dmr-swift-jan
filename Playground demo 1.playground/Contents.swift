f1272313a20135ef5d002ffd10a646e0f8c56765
Ludovic Ollagnier <ludovic@ollagnier.me>
2018-01-30 08:34:53 +0100

    Ajout premiers slides + playground demo

---
+++ Playground demo 1.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+let languageName = "Swift"
+var version = Float(4.0)
+var version2 = 4.0
+let introductionYear = 2014
+let isAwesome = true
+
+let char: Character = "S"
+
+version = Float(version2)
