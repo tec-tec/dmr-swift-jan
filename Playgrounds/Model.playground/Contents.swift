d55d6b14c0a0c5e1cfd72db55f0365401d6ae2f6
Ludovic Ollagnier <ludovic@ollagnier.me>
2018-01-31 11:49:46 +0100

    Implementation Library

--- Playgrounds/Model.playground/Contents.swift
+++ Playgrounds/Model.playground/Contents.swift
@@ -31,16 +31,43 @@ struct Restaurant {
 
 class Library {
 
+    private var restaurants: [Restaurant] = []
+//    var restaurants = [Restaurant]()
+//    var restaurants: Array<Restaurant> = []
+//    var restaurants = Array<Restaurant>()
+
+    func add(_ resto: Restaurant) {
+        restaurants.append(resto)
+    }
+
+//    func list() -> [Restaurant] {
+//        return restaurants
+//    }
+
+    //Computed property get-only
+    var allRestaurants: [Restaurant] {
+        return restaurants
+    }
+//
+//    var allRestaurant2: [Restaurant] {
+//        get {
+//            return restaurants
+//        }
+//        set {
+//            restaurants = newValue
+//        }
+//    }
 }
 
+let dir = Library()
 
+let resto1 = Restaurant(name: "", adress: "", style: RestaurantStyle.chinese, mediumPrice: 10, notation: nil, isVisited: false)
 
+dir.allRestaurants
 
+dir.add(resto1)
 
-
-
-
-
+dir.allRestaurants
 
 
 
