8ff8083d9ff93e7ec9d3feb765bb42c383492523
Ludovic Ollagnier <ludovic@ollagnier.me>
2018-01-31 22:40:07 +0100

    Demo outlet/action

--- Projets/Restos/Restos/ViewController.swift
+++ Projets/Restos/Restos/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var destinationLabel: UILabel!
+    @IBOutlet weak var originTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showTextfieldContent(_ sender: UIButton) {
+
+        sender.isEnabled = false
+        destinationLabel.text = originTextField.text
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
