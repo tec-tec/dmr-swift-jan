eb480e71ae5f441f90b548abe8707f600dfeb2c8
Ludovic Ollagnier <ludovic@ollagnier.me>
2018-02-01 10:43:47 +0100

    Mise en place de l'interface + nettoyage ViewController

--- Projets/Restos/Restos/ViewController.swift
+++ Projets/Restos/Restos/ViewController.swift
@@ -10,23 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var destinationLabel: UILabel!
-    @IBOutlet weak var originTextField: UITextField!
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func showTextfieldContent(_ sender: UIButton) {
-
-        sender.isEnabled = false
-        destinationLabel.text = originTextField.text
     }
 
 }
